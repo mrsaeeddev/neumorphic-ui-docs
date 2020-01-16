47c123662e8ebba667b66e3f64cf169ebabb57cb
sa-js <philipthevictor@gmail.com>
2020-01-16 12:21:48 +0500

    updated NeuButton docs and changed gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Documentation Starter',
-    description: 'This is a starter for gatsby-theme-documentation'
+    siteName: 'neumorhpic-ui',
+    title: 'A library for components based on the concept of neumorphism',
+    siteUrl:'https://neumorphic-ui.netlify.com',
+    lang: 'en',
+    description: 'neumorphism, neumorphic-ui, neumorhpism, skeuomorphism, web, design',
+    keywords: 'neumorphism, neumorphic-ui, neumorhpism, skeuomorphism, web, design'
   },
   plugins: [
     'gatsby-theme-documentation'
